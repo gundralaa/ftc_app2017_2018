3fdffaf2c373fb99d0589243f1470dc81296ed6e
gundralaa <mathgame99@gmail.com>
2017-06-08 17:05:48 -0700

    Hardware Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewClassYas.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewClassYas.java
@@ -8,8 +8,10 @@ import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
 
 /**
  * Created by abhin on 5/30/2017.
@@ -18,16 +20,26 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class NewClassYas {
 
     public DcMotor lMotor, rMotor;
-    public ModernRoboticsI2cColorSensor mrColor;
+    public ColorSensor mrColor;
     public ModernRoboticsI2cRangeSensor mrRange;
     public ModernRoboticsI2cGyro mrGyro;
     public AdafruitBNO055IMU adaGyro;
-    public ModernRoboticsAnalogOpticalDistanceSensor leftOds, rightOds;
+    public OpticalDistanceSensor leftOds, rightOds;
 
     HardwareMap lmap;
 
     public void init(HardwareMap map){
         lmap = map;
+
+        lMotor = lmap.dcMotor.get("lMotor");
+        rMotor = lmap.dcMotor.get("rMotor");
+
+        mrColor = lmap.colorSensor.get("mrColor");
+        mrRange = lmap.get(ModernRoboticsI2cRangeSensor.class,"mrRange");
+        mrGyro = lmap.get(ModernRoboticsI2cGyro.class,"mrGyro");
+
+        leftOds = lmap.opticalDistanceSensor.get("leftOds");
+        rightOds = lmap.opticalDistanceSensor.get("rightOds");
 
 
 
