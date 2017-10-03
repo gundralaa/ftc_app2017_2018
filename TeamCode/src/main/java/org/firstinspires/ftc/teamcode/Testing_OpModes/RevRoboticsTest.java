296f075a2c71b84a7dbd141da05a705f90707545
Danny <saelid2517@gmail.com>
2017-10-03 16:15:42 -0700

    Weird equations for tank driving

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing_OpModes/RevRoboticsTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing_OpModes/RevRoboticsTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by abhin on 10/2/2017.
@@ -11,31 +12,52 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @TeleOp(name = "RevRoboticsTest", group = "")
 public class RevRoboticsTest extends LinearOpMode {
 
-    DcMotor lMotor, rMotor;
+    DcMotor leftMotor, rightMotor;
     double lPower, rPower;
 
 
     @Override
-    public void runOpMode() throws InterruptedException {
-        lMotor = hardwareMap.dcMotor.get("lMotor");
-        rMotor = hardwareMap.dcMotor.get("rMotor");
+    public void runOpMode() {
+        leftMotor = hardwareMap.dcMotor.get("lMotor");
+        rightMotor = hardwareMap.dcMotor.get("rMotor");
 
-        lMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        telemetry.addData("Mode", "Waiting for start...");
+        telemetry.update();
         waitForStart();
+        telemetry.addData("Mode", "Started");
 
-        while (opModeIsActive()){
-            lPower = gamepad1.left_stick_y;
-            rPower = gamepad1.right_stick_y;
-            telemetry.addData("Left Motor Power", lPower);
-            telemetry.addData("Right Motor Power", rPower);
+        while (opModeIsActive()) {
+            lPower = steadyAcceleration(Range.clip(gamepad1.left_stick_y, -1.0, 1.0));
+            rPower = steadyAcceleration(Range.clip(gamepad1.right_stick_y, -1.0, 1.0));
+            telemetry.addData("LeftMotorPower", lPower);
+            telemetry.addData("RightMotorPower", rPower);
             telemetry.update();
 
-            lMotor.setPower(lPower);
-            rMotor.setPower(rPower);
+            leftMotor.setPower(lPower);
+            rightMotor.setPower(rPower);
         }
+
+    }
+
+    public double steadyAcceleration(double raw) {
+        if (raw >= -0.5 && raw <= 0.5) {
+            return linear(raw);
+        }
+        else {
+            return cubic(raw);
+        }
+    }
+
+    public double cubic(double x) {
+        return Math.pow(x, 3.0);
+    }
+
+    public double linear(double x) {
+        return x * 0.25;
     }
 }
