7bbd4e6118b45898bfca7f66a8cb4d1286ed5ce2
gundralaa <mathgame99@gmail.com>
2017-10-23 16:13:57 -0700

    Functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp_OpModes/RedNear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Comp_OpModes/RedNear.java
@@ -6,12 +6,14 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.teamcode.Resources.Functions;
 
 /**
  * Created by abhin on 10/23/2017.
  */
 @Autonomous(name = "RedNear",group = "Auton")
 public class RedNear extends LinearOpMode {
+    final double GREY_THRESHOLD = 0.1;
     RelicRecoveryVuMark seenMark;
     ElapsedTime runtime = new ElapsedTime();
     double timeOutS = 5.0;
@@ -29,18 +31,21 @@ public class RedNear extends LinearOpMode {
         //Loop For a certain amount of time until the Image is Seen
         runtime.reset();
         while (seenMark != RelicRecoveryVuMark.UNKNOWN || runtime.seconds() < timeOutS) { // will break somewhere... hopefully on seeing a recognizable trackable
-            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
-            if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-                seenMark = vuMark;
-                break;
-            }
+            RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(bot.relicTemplate);
+            seenMark = vuMark;
             idle();
         }
 
         //TODO Drive until the light sensor sees a large change in light
-        
+        Functions.setMotors(bot, 0.75);
+        while (bot.colorSensor.alpha() > GREY_THRESHOLD){
+            telemetry.addData("Alpha", bot.colorSensor.alpha());
+            telemetry.update();
+        }
+        Functions.setMotors(bot, 0.0);
 
         //TODO If the seenMark is null then set to a default
+        if(seenMark == )
 
         //TODO Encoder Drive Forward based on the seen Mark
 
