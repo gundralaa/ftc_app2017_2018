153d5795e6e99cf2d4384732c0d520b175e1cf00
Danny <saelid2517@gmail.com>
2018-01-22 15:14:50 -0800

    Mechanum TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanum/HardwareBot.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.Mechanum;
+
+/**
+ * Created by saeli on 1/16/2018.
+ */
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+public class HardwareBot {
+    public VuforiaLocalizer vuforia;
+    public VuforiaTrackables relicTrackables;
+    public DcMotor leftFrontMotor, rightFrontMotor, leftBackMotor, rightBackMotor, linearSlideMotor, relicSlideMotor;
+    public Servo leftGrabServo, rightGrabServo, verticalJewel, horizontalJewel, relicRotater;
+    public BNO055IMU imu;
+    public HardwareMap lmap;
+    public ColorSensor colorSensor;
+    VuforiaTrackable relicTemplate;
+
+    public void init(HardwareMap map) {
+        lmap = map;
+        //Trolled
+        // Color sensor
+        colorSensor = lmap.get(ColorSensor.class, "sensor_color");
+
+        // Motors
+        leftFrontMotor = lmap.dcMotor.get("lFrontMotor");
+        rightFrontMotor = lmap.dcMotor.get("rFrontMotor");
+        leftBackMotor = lmap.dcMotor.get("lBackMotor");
+        rightBackMotor = lmap.dcMotor.get("rBackMotor");
+        linearSlideMotor = lmap.dcMotor.get("linearSlideMotor");
+        relicSlideMotor = lmap.dcMotor.get("relicSlide");
+        leftGrabServo = lmap.servo.get("lServo");
+        rightGrabServo = lmap.servo.get("rServo");
+        verticalJewel = lmap.servo.get("vJewel");
+        horizontalJewel = lmap.servo.get("hJewel");
+        relicRotater = lmap.servo.get("rRotater");
+
+        leftFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+        leftBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        leftBackMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBackMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Vuforia
+        int cameraMonitorViewId = lmap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", lmap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters vufParameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        vufParameters.vuforiaLicenseKey = "AYjW+kn/////AAAAGckyQkdtk0g+vMt7+v21EQwSR82nxrrI34xlR+F75StLY+q3kjvWvgZiO0rBImulRIdCD4IjzWtqgZ8lPunOWuhUUi5eERTExNybyTwhn4GpdRr2XkcN+5uFD+5ZRoMfgx+z4RL4ONOLGWVMD30/VhwSM5vvkKB9C1VyGK0DyKcidSfxW8yhL1BKR2J0B5DtRtDW91hzalAEH2BfKE2+ee/F8f0HQ67DE5nnoVqrnT+THXWFb9W6OOBLszYdHTkUMtMV5U0RQxNuTBkeYGHtgcy17ULkQLY9Lnv0pqCLKdvlz4P3gtUAHPs/kr1cfzcaCS4iRY+ZlwxxLIKSazd0u4NSBjhH/f+zKJMaL/uVG2j4"; // get one
+        vufParameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(vufParameters);
+
+        relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        relicTemplate = relicTrackables.get(0);
+
+        // IMU
+        BNO055IMU.Parameters imuParameters = new BNO055IMU.Parameters();
+        imuParameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imuParameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        imuParameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        imuParameters.loggingEnabled = true;
+        imuParameters.loggingTag = "IMU";
+        imuParameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = lmap.get(BNO055IMU.class, "imu");
+        imu.initialize(imuParameters);
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanum/MechanumTeleOp.java
@@ -0,0 +1,134 @@
+package org.firstinspires.ftc.teamcode.Mechanum;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by saeli on 1/16/2018.
+ */
+
+@TeleOp(name = "Mechanum TeleOp", group = "Mechanum")
+public class MechanumTeleOp extends LinearOpMode {
+    public final double SERVO_INCREMENT = 0.01;
+    public final double LEFT_SERVO_MIN = 0.4;
+    public final double RIGHT_SERVO_MAX = 0.6;
+    double lPower, rPower;
+    double strafingPower = 0.5;
+
+    public void runOpMode() {
+        HardwareBot bot = new HardwareBot();
+        bot.init(hardwareMap);
+
+        telemetry.addData("Mode", "Waiting for start...");
+        telemetry.update();
+        waitForStart();
+        telemetry.addData("Mode", "Running...");
+        telemetry.update();
+        bot.leftGrabServo.setPosition(1.0);
+        bot.rightGrabServo.setPosition(0.0);
+
+        while (opModeIsActive()) {
+            lPower = Range.clip(cubic(gamepad1.left_stick_y), -1.0, 1.0);
+            rPower = Range.clip(cubic(gamepad1.right_stick_y), -1.0, 1.0);
+
+            // Glyph grabbing
+            if (gamepad2.x) {
+                closeServos(bot.leftGrabServo, bot.rightGrabServo);
+            } else if (gamepad2.b) {
+                openServos(bot.leftGrabServo, bot.rightGrabServo);
+            }
+            telemetry.addData("Left Grab Servo", bot.leftGrabServo.getPosition());
+            telemetry.addData("Right Grab Servo", bot.rightGrabServo.getPosition());
+
+            // Linear slide power
+            if (gamepad2.y) {
+                bot.linearSlideMotor.setPower(0.7);
+                telemetry.addData("Glyph Slide Motor", "UP");
+            } else if (gamepad2.a) {
+                bot.linearSlideMotor.setPower(-0.7);
+                telemetry.addData("Glyph Slide Motor", "DOWN");
+            } else {
+                bot.linearSlideMotor.setPower(0.0);
+                telemetry.addData("Glyph Slide Motor", "OFF");
+            }
+
+            // Relic slide power
+            if (gamepad2.left_bumper) {
+                bot.relicSlideMotor.setPower(0.5);
+                telemetry.addData("Relic Slide", "ON");
+            } else {
+                bot.relicSlideMotor.setPower(0.0);
+                telemetry.addData("Relic Slide", "OFF");
+            }
+
+            // Grabber stuff
+            // 0.53994 = down
+            if (gamepad2.dpad_up) {
+                bot.relicRotater.setPosition(Range.clip(bot.relicRotater.getPosition() + 0.005, 0.4, 0.6));
+            } else if (gamepad2.dpad_down) {
+                bot.relicRotater.setPosition(Range.clip(bot.relicRotater.getPosition() - 0.005, 0.4, 0.6));
+            }
+            telemetry.addData("RelicRotater: ", bot.relicRotater.getPosition());
+
+            // Drive base
+            if (gamepad1.left_trigger > 0) { // Strafe left
+                strafingPower = gamepad1.left_trigger;
+                bot.leftFrontMotor.setPower(strafingPower);
+                bot.leftBackMotor.setPower(-strafingPower);
+                bot.rightFrontMotor.setPower(-strafingPower);
+                bot.rightBackMotor.setPower(strafingPower);
+                telemetry.addData("Drive", "Strafing left");
+            } else if (gamepad1.right_trigger > 0) { // Strafe right
+                strafingPower = gamepad1.right_trigger;
+                bot.leftFrontMotor.setPower(-strafingPower);
+                bot.leftBackMotor.setPower(strafingPower);
+                bot.rightFrontMotor.setPower(strafingPower);
+                bot.rightBackMotor.setPower(-strafingPower);
+                telemetry.addData("Drive", "Strafing right");
+            } else { // Tank driving
+                bot.leftFrontMotor.setPower(lPower);
+                bot.leftBackMotor.setPower(lPower);
+                bot.rightFrontMotor.setPower(rPower);
+                bot.rightBackMotor.setPower(rPower);
+                telemetry.addData("Drive", "Left Power - " + lPower + "; Right Power - " + rPower);
+            }
+            telemetry.update();
+            idle();
+        }
+    }
+
+    public double cubic(double x) {
+        return Math.pow(x, 3.0);
+    }
+
+    public void closeServos(Servo left, Servo right) {
+        double currPosL, currPosR;
+        while (gamepad2.x) {
+            currPosL = left.getPosition();
+            currPosR = right.getPosition();
+            telemetry.addData("Left Position: ", currPosL);
+            telemetry.addData("Right Position: ", currPosR);
+            telemetry.update();
+            left.setPosition(Range.clip(currPosL - SERVO_INCREMENT, LEFT_SERVO_MIN, 1.0));
+            right.setPosition(Range.clip(currPosR + SERVO_INCREMENT, 0.0, RIGHT_SERVO_MAX));
+            idle();
+        }
+    }
+
+    // x pressed
+    public void openServos(Servo left, Servo right) {
+        double currPosL, currPosR;
+        while (gamepad2.b) {
+            currPosL = left.getPosition();
+            currPosR = right.getPosition();
+            telemetry.addData("Left Position: ", currPosL);
+            telemetry.addData("Right Position: ", currPosR);
+            telemetry.update();
+            left.setPosition(Range.clip(currPosL + SERVO_INCREMENT, LEFT_SERVO_MIN, 1.0));
+            right.setPosition(Range.clip(currPosR - SERVO_INCREMENT, -1.0, RIGHT_SERVO_MAX));
+            idle();
+        }
+    }
+}
